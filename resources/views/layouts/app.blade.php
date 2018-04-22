e59e4d8c3b1ce5787331587deb08058c2e8e9a8b
Kevin Langlois <kev.langlois@gmail.com>
2018-04-22 15:44:54 -0400

    title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'UpSystem') }}</title>
+    <title>100% Free Upsystem</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
