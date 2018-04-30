263d3d161f635ca99e4969dbcb13662c473a9287
Kevin Langlois <kev.langlois@gmail.com>
2018-04-30 09:27:49 -0400

    test

--- resources/angular/src/app/app.component.ts
+++ resources/angular/src/app/app.component.ts
@@ -129,7 +129,7 @@ export class AppComponent {
                 $('[name="first_name"]').focus();
             });
         });
-        let timestamp = new Date().getTime();
+        let timestamp = new Date().getTime() / 1000;
         this.http.get<any[]>('/api/staff?currentTime=' + timestamp).subscribe(res => {
             for (let member of res) {
                 if (member.is_available) {
